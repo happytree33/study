542e4d6a66354ee0fd57bf4bf2033b5f05b6edff
yoosone93 <yoosone93@naver.com>
2024-08-13 16:40:56 +0900

    asd

--- src/test1/Myclass.java
+++ src/test1/Myclass.java
@@ -1,5 +1,17 @@
 package test1;
 
 public class Myclass {
-
+	private int value;
+	
+	public Myclass(int value) {
+		this.value = value;
+	}
+	
+	@Override
+	public String toString() {
+		System.out.println("test");
+		return "Myclass[value=" + value + "]";
+	}
+	
+	
 }
